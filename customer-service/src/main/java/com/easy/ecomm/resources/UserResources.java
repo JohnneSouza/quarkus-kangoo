15f259f7c495e8d0fa7808cab0ed29fb5f53fe1c
Johnnes Souza <jhonnesf5@gmail.com>
2021-01-25 23:23:42 -0300

    Addded OpenAPI Lib

--- customer-service/src/main/java/com/easy/ecomm/resources/UserResources.java
+++ customer-service/src/main/java/com/easy/ecomm/resources/UserResources.java
@@ -3,17 +3,27 @@ package com.easy.ecomm.resources;
 import com.easy.ecomm.model.User;
 import com.easy.ecomm.model.dto.UserDto;
 import com.easy.ecomm.service.UserService;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
 @Path("/users")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
+@Produces(APPLICATION_JSON)
+@Consumes(APPLICATION_JSON)
 public class UserResources {
 
     @Inject
@@ -21,23 +31,35 @@ public class UserResources {
 
     @POST
     @Transactional
+    @Operation(summary = "Creates a new User")
+    @APIResponse(responseCode = "201", content =
+    @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = UserDto.class, required = true)))
     public Response saveUser(@Valid UserDto user){
         return Response.status(Response.Status.CREATED)
                 .entity(userService.saveUser(user, user.getPassword())).build();
     }
 
     @GET
+    @Operation(summary = "Retrieve all Users")
+    @APIResponse(responseCode = "200", content =
+    @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = UserDto.class)))
     public Iterable<User> findAll(){
         return userService.findAll();
     }
 
     @GET
+    @Operation(summary = "Retrieve a User by its id")
+    @APIResponse(responseCode = "200", content =
+    @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = UserDto.class)))
     @Path("{id}")
     public User findByUserById(@PathParam("id") int id){
         return userService.findUserById(id);
     }
 
     @GET
+    @Operation(summary = "Retrieve a user by its email")
+    @APIResponse(responseCode = "200", content =
+    @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = UserDto.class)))
     @Path("email/{email}")
     public User findUserByEmail(@PathParam("email") String email){
         return userService.finderByEmail(email);
